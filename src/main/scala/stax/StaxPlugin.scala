ea8e37424d1285fb55de1f82ab929a42f7ddb86a
Timothy Perrett <hello@timperrett.com>
2010-03-12 22:44:03 +0000

    Updated code

--- src/main/scala/stax/StaxPlugin.scala
+++ src/main/scala/stax/StaxPlugin.scala
@@ -9,16 +9,52 @@ import java.io.{BufferedReader,File,FileInputStream,FileOutputStream,IOException
 import java.util.zip.{ZipEntry,ZipOutputStream}
 
 trait StaxPlugin extends DefaultWebProject {
-  /** 
-   * deploy to stax.net
+  def staxApplicationId: String
+  def staxUsername: String = ""
+  
+  /**
+   * Deploy
    */
-  // description
-  val StaxDeployDescription = "Deploy your WAR to stax.net with the stax-deploy"
-  // command
-  lazy val staxDeploy = staxDeployAction
-  // action
-  protected def staxDeployAction = task {
-      None
-    } dependsOn(`package`) describedAs(StaxDeployDescription)
+  
+   // description
+   val StaxDeployDescription = "Deploy your WAR to stax.net with the stax-deploy"
+   // command
+   lazy val staxDeploy = staxDeployAction
+   // action
+   protected def staxDeployAction = 
+     task(staxDeployTask) dependsOn(`package`) describedAs(StaxDeployDescription)
+
+   private def getCredentials: (String,String) = {
+     val username = if(staxUsername.isEmpty){
+       val un = prompt("Stax Username:")
+       un
+     } else staxUsername
+     val password = prompt("Stax Password:")
+     (username,password)
+   }
+   
+   private def staxDeployTask = {
+     
+     // println(warPath)
+     val appId = prompt("Stax Application ID:")
+     val credentials = getCredentials
+     val client = new StaxClient(apiUrl, credentials._1, credentials._2, "xml", "0.1");
+     client.applicationDeployWar(appId, 
+       this.environment, this.message, 
+       this.deployFile.getAbsolutePath(), 
+       null, new HashWriteProgress());
+     
+     
+     None
+   }
+  
+  /**
+   * Utils
+   */
+  
+  private def trim(s: Option[String]) = s.getOrElse("")
+  private def prompt(withText: String): String = 
+    trim(SimpleReader.readLine("\n"+withText))
+  
   
 }
