9de9a1c184c16c52b50a0dca8d0bc0af0f40d168
timperrett <hello@timperrett.com>
2011-04-19 00:25:26 +0100

    Removed kruft comments and added posterus plugin back in

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,18 +1,11 @@
 import sbt._
 
-class CloudBeesPlugin(info: ProjectInfo) extends PluginProject(info){ // with posterous.Publish {
+class CloudBeesPlugin(info: ProjectInfo) extends PluginProject(info) with posterous.Publish {
   // repositories
-  // val beesRepo = "stax.repo" at "http://repository-cloudbees.forge.cloudbees.com/public-snapshot/"
   val specsRepo = "specs.repo" at "http://specs.googlecode.com/svn/maven2/"
   val sonatypeRepo = "sonatype.repo" at "https://oss.sonatype.org/content/groups/public"
   
   // dependencies
-  // val apiclient = "net.stax" % "stax-api-client" % "1.0.20110131-SNAPSHOT" % "compile"
-  // val appserver = "net.stax" % "stax-appserver" % "1.0.20110131-SNAPSHOT" % "compile"
-  // val stax = "stax" % "stax-api" % "1.0.1" % "compile"
-  // val xstream = "com.thoughtworks.xstream" % "xstream" % "1.3" % "compile"
-  // val httpclient = "commons-httpclient" % "commons-httpclient" % "3.1" % "compile"
-  // val jetison = "org.codehaus.jettison" % "jettison" % "1.0.1" % "compile"
   val cbclient = "com.cloudbees" % "cloudbees-api-client-nodeps" % "1.0.0-SNAPSHOT" % "compile"
   
   // testing
